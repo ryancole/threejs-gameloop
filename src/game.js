6b022e2d4250aff5985362bf232645f33539a852
Ryan Cole <ryan@rycole.com>
2013-04-09 19:06:04 -0500

    idk

--- src/game.js
+++ src/game.js
@@ -1,10 +1,8 @@
 
 function Game () {
 
-    var clientRect = document.body.getBoundingClientRect();
-
-    var sceneWidth = clientRect.width,
-        sceneHeight = clientRect.height;
+    var sceneWidth = window.innerWidth,
+        sceneHeight = window.innerHeight;
 
     var cameraAngle = 45,
         cameraNear = 0.1,
@@ -22,29 +20,50 @@ function Game () {
 
     // init the camera
     this.camera = new THREE.PerspectiveCamera(cameraAngle, cameraAspectRatio, cameraNear, cameraFar);
-    this.camera.position.z = 600;
 
     // init the scene
     this.scene = new THREE.Scene;
     this.scene.add(this.camera);
 
+    // init the controls
+    this.controls = new THREE.TrackballControls( this.camera );
+
     document.body.appendChild(this.renderer.domElement);
     document.body.appendChild(this.stats.domElement);
 
-    this.animate();
+    var cubeGeometry = new THREE.CubeGeometry( 50, 50, 50 );
+    var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x000088 } );
+    var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+    cube.position.set(0,26,0);
+    this.scene.add(cube);
+
+    this.camera.position.set(0, 150, 400);
+    this.camera.lookAt(this.scene.position);
+
+};
+
+Game.prototype.update = function update () {
+
+    this.controls.update();
+    this.stats.update();
+
+};
+
+Game.prototype.render = function render () {
+
+    this.renderer.render(this.scene, this.camera);
 
 };
 
 Game.prototype.animate = function animate () {
 
-    this.stats.begin();
-
-    // render the current scene
-    this.renderer.render(this.scene, this.camera);
-
-    this.stats.end();
-
     // request the next animation frame
     requestAnimationFrame(this.animate.bind(this));
+
+    // render the current scene
+    this.render();
+
+    // perform updates for the next scene
+    this.update();
 
 };
